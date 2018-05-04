e2de9f9e8c057c27dab311ad5a7558f71829cdbf
Sophia Varella <sophiavarella@gmail.com>
2018-05-03 18:06:03 -0700

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,23 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets]
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.newTweets.length > 0) {
+      return true
+    }
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
